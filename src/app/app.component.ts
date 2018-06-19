d387096356775cb4f2a22e1684da3b581456bb63
Mohammad Asl Vakil <m.vakili95@gmail.com>
2018-06-19 13:04:44 +0430

    fix: minor ui features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,6 +37,64 @@ export class AppComponent {
         'awe', 'cxcbb'
 
       ]
+    },
+    {
+      Id: '1',
+      Name: 'Nabi',
+      Phone: '3221',
+      Data: [
+        'small', 'medium', 'large'
+      ],
+      Data2: [
+        'Men', 'Women'
+      ]
+    },
+    {
+      Id: '2',
+      Name: 'N1121abi',
+      Phone: '564',
+      Data: [
+        '55555', '5', '0gfh',
+        '55555', '5', '0gfh',
+        '55555', '5', '0gfh',
+        '55555', '5', '0gfh',
+        '55555', '5', '0gfh',
+        '55555', '5', '0gfh'
+      ],
+      Data2: [
+        'awe', 'cxcbb',
+        'awe', 'cxcbb'
+
+      ]
+    },
+    {
+      Id: '1',
+      Name: '41521314',
+      Phone: '3221',
+      Data: [
+        'small', 'me52542dium', 'larg452e'
+      ],
+      Data2: [
+        'Men', 'Women', 'Men', 'Women'
+      ]
+    },
+    {
+      Id: '456423',
+      Name: 'asdsaasdas',
+      Phone: '51231364',
+      Data: [
+        '55555', '5', '0gfh',
+        'ss', '5', '0gfh', 'asdasd',
+        '55555', '5', '0gfh',
+        '555a55', '5', '0gfh',
+        '55555', '5', '0gfh',
+        '55555', '5', '0gfh'
+      ],
+      Data2: [
+        'awe', 'cxcbb',
+        'awe', 'cxcbb'
+
+      ]
     }
   ];
 
