980d4e4721780f556c399deb1996bc4d53734243
Muhammad Vakili <m.vakili95@gmail.com>
2018-06-29 15:02:41 +0430

    transfer to noushmac

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,88 +12,88 @@ export class AppComponent {
     {
       Id: '1',
       Name: 'Nabi',
-      Phone: '3221',
-      Data: [
+      Phone: '+12 345 678',
+      Size: [
         'small', 'medium', 'large'
       ],
-      Data2: [
-        'Men', 'Women'
+      Type: [
+        'Man', 'Woman'
       ]
-    },
-    {
+    }, {
       Id: '2',
-      Name: 'N1121abi',
-      Phone: '564',
-      Data: [
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh'
+      Name: 'Noushmac',
+      Phone: '+52 221 983',
+      Size: [
+        'small', 'medium', 'large', 'x large'
       ],
-      Data2: [
-        'awe', 'cxcbb',
-        'awe', 'cxcbb'
-
+      Type: [
+        'Man', 'Woman', 'Child'
       ]
     },
     {
-      Id: '1',
-      Name: 'Nabi',
-      Phone: '3221',
-      Data: [
-        'small', 'medium', 'large'
+      Id: '3',
+      Name: 'Kazim',
+      Phone: '+80 235 874',
+      Size: [
+        'small'
       ],
-      Data2: [
-        'Men', 'Women'
+      Type: [
+        'Man', 'NewBorn'
       ]
     },
     {
-      Id: '2',
-      Name: 'N1121abi',
-      Phone: '564',
-      Data: [
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh'
+      Id: '4',
+      Name: 'Davood',
+      Phone: '+73 214 365',
+      Size: [
+        'small', 'large'
       ],
-      Data2: [
-        'awe', 'cxcbb',
-        'awe', 'cxcbb'
-
+      Type: [
+        'Man', 'Woman'
       ]
     },
     {
-      Id: '1',
-      Name: '41521314',
-      Phone: '3221',
-      Data: [
-        'small', 'me52542dium', 'larg452e'
+      Id: '5',
+      Name: 'Mammad',
+      Phone: '+21 332 236',
+      Size: [
+        'small', 'large', 'x small'
       ],
-      Data2: [
-        'Men', 'Women', 'Men', 'Women'
+      Type: [
+        'Man', 'Woman'
       ]
     },
     {
-      Id: '456423',
-      Name: 'asdsaasdas',
-      Phone: '51231364',
-      Data: [
-        '55555', '5', '0gfh',
-        'ss', '5', '0gfh', 'asdasd',
-        '55555', '5', '0gfh',
-        '555a55', '5', '0gfh',
-        '55555', '5', '0gfh',
-        '55555', '5', '0gfh'
+      Id: '6',
+      Name: 'Sarah',
+      Phone: '+21 324 236',
+      Size: [
+        'small'
       ],
-      Data2: [
-        'awe', 'cxcbb',
-        'awe', 'cxcbb'
-
+      Type: [
+        'Woman'
+      ]
+    },
+    {
+      Id: '7',
+      Name: 'Davood',
+      Phone: '+73 214 365',
+      Size: [
+        'small', 'large'
+      ],
+      Type: [
+        'Man', 'Woman'
+      ]
+    },
+    {
+      Id: '8',
+      Name: 'Davood',
+      Phone: '+73 214 365',
+      Size: [
+        'small', 'large'
+      ],
+      Type: [
+        'Man', 'Woman'
       ]
     }
   ];
